4413c6ea0562a8f1da44a2b127fcd4e8e453ebe9
Vlad Rotariu <vladrotariu24@gmail.com>
2021-06-02 20:21:35 +0300

    updated database credentials

--- database/db_singleton.py
+++ database/db_singleton.py
@@ -6,10 +6,10 @@ class Database:
     def __new__(self):
         if self.__db is None:
             self.__db = mysql.connector.connect(
-                host="localhost",
-                user="root",
-                password="",
-                database="unemployment_statistics"
+                host="eu-cdbr-west-01.cleardb.com",
+                user="b7661d429b4c38",
+                password="ef309c34",
+                database="heroku_b5f761ddd8a2ef8"
             )
         return self.__db
 
